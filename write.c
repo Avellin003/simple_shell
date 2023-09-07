b48008197b45fbe9ec631c7999fad6f59f3cb318
Avellin003 <uavellinbonaparte@gmail.com>
2023-09-07 07:24:31 -0700

    update

---
+++ write.c
@@ -0,0 +1,10 @@
+#include "shell.h"
+/**
+ * writer - function that writes to stdout
+ * @input: argument holding the input command
+ * Return: nothing
+ */
+void writer(const char *input)
+{
+	write(1, input, strlen(input));
+}
