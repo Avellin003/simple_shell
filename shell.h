60c9c3319bb4dbe606cef996bc7266c80a7d9974
Avellin003 <uavellinbonaparte@gmail.com>
2023-09-07 08:18:01 +0200

    writer and reader

---
+++ shell.h
@@ -0,0 +1,8 @@
+#ifndef SHELL_H
+#define SHELL_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+void get_input(int argc, char *argv,char inputp[120]);
+void writer(const char *input);
+#endif
